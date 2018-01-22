289c6e0c51d84de5dcbf7daf9c04b897ba821698
Antal Tettinger <antal.tettinger@gmail.com>
2018-01-22 10:06:37 +0800

    Game finished
    No errors, handles all cases

--- app.js
+++ app.js
@@ -8,3 +8,102 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+var scores, roundScore, activePlayer, gamePlaying;
+
+gamePlaying = true;
+
+init();
+//0 first player 1 second player - associated with score array 0 and 1
+
+//document.querySelector('#current-' + activePlayer).textContent = dice;
+
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+//var x = document.querySelector('score-0').textContent; //store value of dice roll
+
+
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+    if(gamePlaying){
+
+        //Random number
+        var dice = Math.floor(Math.random()*6) + 1;
+        //Display results
+        let diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-' + dice + '.png';
+
+        if(dice !== 1){
+            //add score
+            roundScore += dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        } else {
+            //next player
+            /*activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+            roundScore = 0;
+            document.getElementById('current-0').textContent = '0';
+            document.getElementById('current-1').textContent = '0';
+            //make it visible who is thee active player. red dot, grey background, done by the "active" class.
+            document.querySelector('.player-0-panel').classList.toggle('active');
+            document.querySelector('.player-1-panel').classList.toggle('active');
+
+            document.querySelector('.dice').style.display = 'none'; */
+            nextPlayer();
+        }
+    }
+    
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    if(gamePlaying){
+        //Add current score to global score
+        scores[activePlayer] += roundScore;
+        //Update the UI 
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+        //Check if player won the game
+        if(scores[activePlayer] >= 20){
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            gamePlaying = false;
+        } else{
+        nextPlayer();
+            }
+    }
+   
+});
+
+function nextPlayer(){
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    //make it visible who is thee active player. red dot, grey background, done by the "active" class.
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display = 'none';
+}
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init(){
+    scores = [0, 0],
+    roundScore = 0;
+    activePlayer = 0;
+
+    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+}
